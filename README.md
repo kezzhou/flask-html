1c73b8948d9143404918902f572e85b9275ac388
kezzhou <102555416+kezzhou@users.noreply.github.com>
2022-09-14 20:59:47 -0400

    Update 1
    Updated readme with instructions, added a requirements.txt, added template and static folder, created app.py, populated files with examples to test run

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flask-html
 HHA 504 // Week 3 // Assignment 3 // Part 1
+
+## In this repo we take a jump from deploying a flask app locally and remotely to deploying one that is a little more structured with html and a little more beautified with css.
+
+## This repo's flask app is manually styled. For a flask app that is more custom designed with Bootstrap, see: https://github.com/kezzhou/flask-bootstrap
+
+## The focus of this repo will be on styling and structure. Functionality is not yet a primary focus.
+
+## Standard templates and static folders are provided, with .html and .css folders populating them, respectively. We'll keep the app.py file "out in the open" so that it can identify the static and templates folders more easily.
---
+++ app.py
@@ -0,0 +1,40 @@
+#### Terminal commands ####
+
+'pip install -r requirements' ## ensures requirements are installed
+
+'apt-get install updates' ## installs updates for vm
+
+'sudo apt install python3-pip' ##installs pip 
+
+'python3 app.py' ## runs the application
+
+'sudo nohup python3 app.py > log.txt 2>&1 &' ## this terminal command ensures that the app runs in the background even when the client is shut down
+
+
+
+
+#### Imports ####
+
+from flask import Flask, render_template
+
+
+
+
+#### Styling flask with html ####
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+@app.route('/theleech')
+def theleech():
+    return render_template('theleech.html')
+
+@app.route('/songsoftheweek')
+def songsoftheweek():
+    return render_template('songsoftheweek.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
